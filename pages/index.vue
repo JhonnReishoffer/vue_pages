a8923d47aec229edd283d3f78464c295c954d8cb
Jhonnatan <jhonnatan.muniz@soulasalle.com.br>
2025-07-12 21:50:23 -0300

    Fix some visual issues

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,7 @@
-
-
 <template>
-{
-    <script setup lang="ts">
-import { computed } from 'vue'
-import { useWindowSize } from '@vueuse/core'
-
-const { width } = useWindowSize()
-
-const isMobile = computed(() => width.value < 640)
-
-const bannerSrc = computed(() =>
-  isMobile.value
-    ? return (
-        <VendasDiretasSection />
-    )
-    : return (
-        <VendasDiretasSectionMobile />
-    )
-)
-</script>
-}
+  <VendasDiretasSection />
 </template>
 
 <script setup lang="ts">
     import VendasDiretasSection from '@/components/content/VendasDiretasSection.vue'
-    import VendasDiretasSectionMobile from '@/components/content/VendasDiretasSectionMobile.vue'
 </script>
